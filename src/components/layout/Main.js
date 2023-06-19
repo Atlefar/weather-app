385a4921ad57429c19225042354cba1e313bf4a1
atlefar <atle.carlsen@live.no>
2023-06-19 11:29:28 +0200

    La til vær ikon som oppdaterer seg dynamisk etter symbol_code fra apiet

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -3,6 +3,8 @@ import classes from './Main.module.css';
 import Card from '../UI/Card';
 import axios from 'axios';
 
+const images = require.context('../../images/weather', true)
+
 const Main = () => {
     const [data, setData] = useState('');
     // Fetch weather data
@@ -20,24 +22,29 @@ const Main = () => {
         fetchData();
       }, []);
 
-    // add '%' after humidity
+    // add '%' after humidity level
     const relativeHumidity = data?.properties?.timeseries?.[0]?.data?.instant?.details?.relative_humidity;
     const formattedHumidity = relativeHumidity ? `${relativeHumidity}%` : '';
 
-    // add 'm/s' after wind
+    // add 'm/s' after wind level
     const windSpeed = data?.properties?.timeseries?.[0]?.data?.instant?.details?.wind_speed;
     const formattedWindSpeed = windSpeed ? `${windSpeed} m/s` : '';
+
+    // Construct image path dynamically based on the symbol_code from API 
+    const symbolCode = data?.properties?.timeseries?.[0]?.data?.next_1_hours?.summary?.symbol_code;
+    const imagePath = symbolCode ? images(`./${symbolCode}.png`) : null;
 
     // render weather data
     return (
         <div className={classes.container}>
-            {data && data.properties.timeseries && (
-        <Fragment>
-          <h3>{data.properties.timeseries[0].data.instant.details.air_temperature}°</h3>
+            {data && data?.properties?.timeseries && (
+              <Fragment>
+          {imagePath && <img src={imagePath} className={classes.current__weather__icon} alt="Weather" />}
+          <h3>{data?.properties?.timeseries[0]?.data?.instant?.details?.air_temperature}°</h3>
           <Card 
             firstItem={formattedHumidity}
             secondItem={formattedWindSpeed}
-            thirdItem={data.properties.timeseries[0].data.instant.details.ultraviolet_index_clear_sky}
+            thirdItem={data?.properties?.timeseries[0]?.data?.instant?.details?.ultraviolet_index_clear_sky}
             firstIcon={'💧'}
             secondIcon={'↗'}
             thirdIcon={'🌞'}
@@ -45,7 +52,6 @@ const Main = () => {
             secondTitle={'Wind'}
             thirdTitle={'UV-index'}
           />
-          <Card />
         </Fragment>
       )}
         </div>
